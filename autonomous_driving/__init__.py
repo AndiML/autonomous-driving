8ed06793d059a1d48ce6127bf351f88b75377d43
Andreas Lutz <andreasl.95@web.de>
2025-01-30 16:18:20 +0100

    Added main method for execution of the application

---
+++ autonomous_driving/__init__.py
@@ -0,0 +1,7 @@
+"""Represents a package that contains experiments concering percpetion in Autonomous Driving."""
+
+from autonomous_driving.application import Application, __version__
+
+__all__ = [
+    'Application'
+]
---
+++ autonomous_driving/__main__.py
@@ -0,0 +1,6 @@
+"""Represents a module that contains the entrypoint to the application."""
+
+from autonomous_driving import Application
+
+APPLICATION = Application()
+APPLICATION.run()
